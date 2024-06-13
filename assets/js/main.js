75e0ea63ec46679b526d2e3d10682879c8607d5d
Dmytro Didenko <dmitriydidenko02@gmail.com>
2024-06-13 15:53:44 +0300

    mob

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,5 +26,53 @@ document.addEventListener('DOMContentLoaded', function() {
     scrollToSection('.footer__title_steps');
     scrollToSection('.offer__btn_first');
     scrollToSection('.offer__btn_second');
+    scrollToSection('.menu__item_first');
+    scrollToSection('.menu__item_second');
+    scrollToSection('.menu__item_third');
+
+    scrollToSection('.product__more_1');
+    scrollToSection('.product__more_2');
+    scrollToSection('.product__more_3');
+    scrollToSection('.product__more_4');
+    scrollToSection('.product__more_5');
+
 });
 
+//burger
+
+$(document).ready(function(){
+    $('.header__menu').click(function(event){
+      $('.menu').toggleClass('active__menu');
+  
+    });
+  
+  });
+  $(document).ready(function(){
+    $('.header__menu').click(function(event){
+      var menuImage = $('.burger__img');
+      
+      if (menuImage.attr('src') === 'assets/img/burger.png') {
+          menuImage.attr('src', 'assets/img/close_burger.png');
+      } else {
+          menuImage.attr('src', 'assets/img/burger.png');
+      }
+    });
+  });
+  $(document).ready(function(){
+    $('.header__menu_mod').click(function(event){
+      $('.menu__mod').toggleClass('active__menu_mod');
+  
+    });
+  
+  });
+  $(document).ready(function(){
+    $('.header__menu_mod').click(function(event){
+      var menuImage = $('.burger__img_mod');
+      
+      if (menuImage.attr('src') === 'assets/img/burger__black.png') {
+          menuImage.attr('src', 'assets/img/close_burger.png');
+      } else {
+          menuImage.attr('src', 'assets/img/burger__black.png');
+      }
+    });
+  });
